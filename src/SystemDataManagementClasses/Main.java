9982a9e1df05a91f1e20d380bd0dfc5077c3067b
Jean Paul Iradukunda <jeanpaul.iradukunda@eagles.oc.edu>
2016-06-16 11:26:21 -0500

    Added the test data folder as "Data" to the project.

--- src/SystemDataManagementClasses/Main.java
+++ src/SystemDataManagementClasses/Main.java
@@ -11,26 +11,34 @@ public class Main {
 		// TODO Auto-generated method stub
 		try {
 			CsvReader file;
-			/*System.out.println(" ---------------------- University -------------------");
-			file = new CsvReader("/Users/olivier.tuyishime/Google Drive/Summer project I/Test Data/TestData/TestDataUniversityName.csv");
+			
+			/*
+			System.out.println(" ---------------------- University -------------------");
+			file = new CsvReader("src/Data/TestDataUniversityName.csv");
 			UniversityDM univ = new UniversityDM();
 			univ.readFile(file);
-			System.out.println();*/
+			System.out.println();
+			*/
 			
-			/*System.out.println(" ---------------------- Semesters --------------------");
-			file = new CsvReader("/Users/olivier.tuyishime/Google Drive/Summer project I/Test Data/TestData/TestDataSemesters.csv");
+			/*
+			System.out.println(" ---------------------- Semesters --------------------");
+			file = new CsvReader("src/Data/TestDataSemesters.csv");
 			SemesterDM sem = new SemesterDM(file);
 			System.out.println(sem.getSemesterData().size());
-			System.out.println();*/
+			System.out.println();
+			*/
 			
-			/*System.out.println(" ---------------------- Grad Schools --------------------");
-			file = new CsvReader("/Users/olivier.tuyishime/Google Drive/Summer project I/Test Data/TestData/TestDataGradSchools.csv");
+			/*
+			System.out.println(" ---------------------- Grad Schools --------------------");
+			file = new CsvReader("src/Data/TestDataGradSchools.csv");
 			GradSchoolDM gradSchl = new GradSchoolDM();
 			gradSchl.readFile(file);
-			System.out.println();*/
+			System.out.println();
+			*/
+			
 			
 			System.out.println(" ---------------------- Faculty --------------------");
-			file = new CsvReader("/Users/olivier.tuyishime/Google Drive/Summer project I/Test Data/TestData/TestDataFaculty.csv");
+			file = new CsvReader("src/Data/TestDataFaculty.csv");
 			FacultyDM faculty = new FacultyDM(file);
 			
 			Semester sem = new Semester("2016SU");
@@ -44,24 +52,30 @@ public class Main {
 			}
 			System.out.println();
 			
-			/*System.out.println(" ---------------------- Degrees --------------------");
-			file = new CsvReader("/Users/olivier.tuyishime/Google Drive/Summer project I/Test Data/TestData/TestDataDegrees.csv");
+			
+			/*
+			System.out.println(" ---------------------- Degrees --------------------");
+			file = new CsvReader("src/Data/TestDataDegrees.csv");
 			DegreePlanDM degrees = new DegreePlanDM();
 			degrees.readFile(file);
 			System.out.println();
+			*/
 			
+			/*
 			System.out.println(" ---------------------- Degree Requirements--------------------");
-			file = new CsvReader("/Users/olivier.tuyishime/Google Drive/Summer project I/Test Data/TestData/TestDataDegreePlanReq.csv");
+			file = new CsvReader("src/Data/TestDataDegreePlanReq.csv");
 			DegreePlanReqDM degreeReq = new DegreePlanReqDM();
 			degreeReq.readFile(file);
 			System.out.println();
 			*/
-			/*System.out.println(" ---------------------- Courses--------------------");
-			file = new CsvReader("/Users/olivier.tuyishime/Google Drive/Summer project I/Test Data/TestData/TestDataCourses.csv");
+			
+			/*
+			System.out.println(" ---------------------- Courses--------------------");
+			// file = new CsvReader("/Users/olivier.tuyishime/Google Drive/Summer project I/Test Data/TestData/TestDataCourses.csv");
+			file = new CsvReader("src/Data/TestDataCourses.csv");
 			CourseDM courses = new CourseDM(file);
-			
-			System.out.println(courses.getCourses().get(0).getPrerequisites().get(0).getCourseName());*/
-			
+			System.out.println(courses.getCourses().get(0).getPrerequisites().get(0).getCourseName());
+			*/
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
