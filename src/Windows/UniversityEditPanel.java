9757e307d1aaf698f4ba45d2fc707bf1141e8620
Jean Paul Iradukunda <jeanpaul.iradukunda@eagles.oc.edu>
2016-06-20 14:29:57 -0500

    Added the ability to maintain information about a university (name and abbreviation)

--- src/Windows/UniversityEditPanel.java
+++ src/Windows/UniversityEditPanel.java
@@ -18,41 +18,63 @@ import java.awt.event.ActionEvent;
 
 public class UniversityEditPanel extends JPanel {
 	private JTextField txt_universityName;
+	private JTextField text_abbreviation;
 
 	/**
 	 * Create the panel.
 	 */
 	public UniversityEditPanel(JFrame currentFrame, University university) {
 		setBounds(new Rectangle(0, 0, 700, 600));
-		
 		setLayout(null);
-
 		
 		JLabel lblEditUniversityInformation = new JLabel("Edit University Information");
 		lblEditUniversityInformation.setBounds(new Rectangle(200, 50, 300, 30));
 		lblEditUniversityInformation.setFont(new Font("Lucida Grande", Font.BOLD, 18));
 		add(lblEditUniversityInformation);
 		
+		// label and text field for university name
 		JLabel lblName = new JLabel("Name:");
-		lblName.setBounds(new Rectangle(150, 150, 50, 25));
+		lblName.setBounds(new Rectangle(150, 150, 100, 25));
 		add(lblName);
 		
 		txt_universityName = new JTextField();
-		txt_universityName.setText("Oklahoma Chrisrian University");
-		txt_universityName.setBounds(200, 150, 250, 25);
+		txt_universityName.setText(university.getName());
+		txt_universityName.setBounds(250, 150, 250, 25);
 		add(txt_universityName);
 		txt_universityName.setColumns(10);
+		
+		// label and text field for university abbreviation
+		JLabel lblAbbreviation = new JLabel("Abbreviation");
+		lblAbbreviation.setBounds(150, 200, 100, 25);
+		add(lblAbbreviation);
+		
+		text_abbreviation = new JTextField(university.getAbbreviation());
+		text_abbreviation.setBounds(250, 200, 130, 25);
+		add(text_abbreviation);
+		text_abbreviation.setColumns(10);
 		
 		// Save button - it saves the edited university information
 		JButton btn_saveUniversityEdit = new JButton("Save");
 		btn_saveUniversityEdit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				// TODO - get the new name and code and save it
+				// add error handling - when the field is left empty
+				String newName = txt_universityName.getText();
+				university.setName(newName);
+				
+				String newAbbreviation = text_abbreviation.getText();
+				university.setAbbreviation(newAbbreviation);
+				
+				System.out.println("New name - " + newName);
+				System.out.println("New abbr - " + newAbbreviation);
+				
 				currentFrame.getContentPane().removeAll();
 				currentFrame.getContentPane().add(new MainJPanel(university));
 				currentFrame.revalidate();	
 			}
 		});
-		btn_saveUniversityEdit.setBounds(204, 222, 100, 30);
+		btn_saveUniversityEdit.setBounds(204, 300, 100, 30);
 		add(btn_saveUniversityEdit);
 		
 		// Cancel button - it cancels the editing mode of the university information
@@ -64,8 +86,7 @@ public class UniversityEditPanel extends JPanel {
 				currentFrame.revalidate();	
 			}
 		});
-		btn_cancelUniversityEdit.setBounds(316, 222, 100, 30);
+		btn_cancelUniversityEdit.setBounds(316, 300, 100, 30);
 		add(btn_cancelUniversityEdit);
-
 	}
 }
