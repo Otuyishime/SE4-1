5529240ce1f9e15e00761b20a52e8077e084d32d
Gustavemugabo <mugustave@gmail.com>
2016-07-05 20:52:46 -0500

    This commit contains: Login and user Authentication screen and functionalities.{ The default username is :"Admin" and password is:Admin}

---
+++ src/Windows/LoginJDialog.java
@@ -0,0 +1,171 @@
+package Windows;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.border.EmptyBorder;
+
+import OcGraduateSystemClasses.University;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+
+public class LoginJDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JLabel lblImportStudentCourse;
+	private JTextField textFieldUsername;
+	private JTextField jPassword;
+	static University university;
+	static //static SystemJFrame frame = new SystemJFrame(university);
+	boolean succeeded = false;
+
+
+    public  boolean isSucceeded() {
+        return succeeded;
+    }
+
+
+	/**
+	 * Launch the application.
+	 */
+//	public static void main(String[] args) {
+//		try {
+//			LoginJDialog dialog = new LoginJDialog(frame,university );
+//			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+//			dialog.setVisible(true);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+
+	/**
+	 * Create the dialog.
+	 * @param university 
+	 * @param currentFrame 
+	 */
+	public LoginJDialog(JFrame currentFrame, University university) {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.NORTH);
+		{
+			lblImportStudentCourse = new JLabel("LOGIN");
+		}
+		
+		JLabel lblUserName = new JLabel("User Name");
+		
+		JLabel lblNewLabel = new JLabel("Password");
+		
+		textFieldUsername = new JTextField();
+		textFieldUsername.setColumns(10);
+		
+		jPassword = new JPasswordField();
+		jPassword.setColumns(10);
+		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
+		gl_contentPanel.setHorizontalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGap(85)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addComponent(lblNewLabel)
+							.addPreferredGap(ComponentPlacement.RELATED, 85, Short.MAX_VALUE)
+							.addComponent(jPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(Alignment.TRAILING, gl_contentPanel.createSequentialGroup()
+							.addComponent(lblUserName)
+							.addPreferredGap(ComponentPlacement.RELATED, 76, Short.MAX_VALUE)
+							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblImportStudentCourse)
+								.addComponent(textFieldUsername, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+					.addGap(98))
+		);
+		gl_contentPanel.setVerticalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addGap(16)
+					.addComponent(lblImportStudentCourse)
+					.addGap(37)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.TRAILING)
+						.addComponent(textFieldUsername, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblUserName))
+					.addGap(18)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(jPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel))
+					.addContainerGap(90, Short.MAX_VALUE))
+		);
+		contentPanel.setLayout(gl_contentPanel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton logInButton = new JButton("Login");
+				logInButton.setActionCommand("OK");
+				buttonPane.add(logInButton);
+				getRootPane().setDefaultButton(logInButton);
+				
+				logInButton.addActionListener(new ActionListener(){
+					
+
+					public void actionPerformed(ActionEvent e) {
+		                if (LoginJDialog.authenticate(getUsername(), getPassword())) {
+		                    JOptionPane.showMessageDialog(LoginJDialog.this,
+		                            "Hi " + getUsername() + "! You have successfully logged in.",
+		                            "Login",
+		                            JOptionPane.INFORMATION_MESSAGE);
+		                    succeeded  = true;
+		                    
+		                    dispose();
+		                } else {
+		                    JOptionPane.showMessageDialog(LoginJDialog.this,
+		                            "Invalid username or password",
+		                            "Login",
+		                            JOptionPane.ERROR_MESSAGE);
+		                    // reset username and password
+		                    textFieldUsername.setText("");
+		                    jPassword.setText("");
+		                    succeeded = false;
+
+		                }
+		            }
+					
+					
+				});
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+	public static boolean authenticate(String username, String password) {
+      // hard coded user name and password
+        if (username.equals("Admin") && password.equals("Admin")) {
+            return true;
+        }
+        return false;
+    }
+	public String getUsername() {
+        return textFieldUsername.getText().trim();
+    }
+ 
+    public String getPassword() {
+        return new String(((JPasswordField) jPassword).getPassword());
+    }
+ 
+	
+}
--- src/test/Main.java
+++ src/test/Main.java
@@ -12,11 +12,11 @@ import java.util.ArrayList;
 import javax.swing.JDialog;
 
 public class Main {	
-	
+
 	// TODO - we need to import and read the university name inside the UniversityDM
 	// that way, we won't need to have the loadUniversitymain(). 
 	// We will just create an instance of University and be done here. 
-	
+
 	static UniversityDM universityDM;
 	static University university;
 	public static void loadUniversitymain() {
@@ -25,62 +25,52 @@ public class Main {
 			file = new CsvReader("src/Data/TestDataUniversityName.csv");
 			universityDM = new UniversityDM(file);
 			university = universityDM.getUniversity();
-			
+
 			// Import Students and student courses
 			String studentsfilelocation = "src/Data/STU.DUMP.csv";
 			String studentcoursesfilelocation = "src/Data/STC.DUMP.csv";
-			
+
 			universityDM.importStudentCourses(studentcoursesfilelocation);
 			universityDM.importStudents(studentsfilelocation);
-			
-		//	System.out.println("++++++++++++++++++++++++ PRINT FROM ALL OBJECTS ++++++++++++++++++++++++");
-			
+
+			//	System.out.println("++++++++++++++++++++++++ PRINT FROM ALL OBJECTS ++++++++++++++++++++++++");
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					
+
 					// create university
 					loadUniversitymain();
+
 					
-//					for ( Course course : university.generateCoursesNeededByStudentsInDegree(university.getDegrees().get(0), university.getSemesters().get(1))){
-//						course.print();
-//						System.out.print("Number of Students who need the course: ");
-//						System.out.println(university.getNumberStudentsNeedingCourse(university.getStudentsInDegree(university.getDegrees().get(0).getDegreeCode()), course));
-//						System.out.println("professor load available: " + course.getFaculties().get(0).getSummerLoad());
-//						System.out.println("=============================================================================================================");
-//					}
-//					System.out.println();
-//					System.out.println("Total number of students to get the courses: " + university.getStudentsInDegree(university.getDegrees().get(0).getDegreeCode()).size());
-					
-					
-//					ArrayList<Section> sections = university.generateSectionsFromNeededCourses(university.getDegrees().get(0), university.getSemesters().get(1));
-//					System.out.println();
-//					System.out.println("========================================================== SCHEDULE ================================================================");
-//					for (Section section : sections){
-//						System.out.println(section);
-//					}
-					
-				//	Schedule schedule = university.generateSchedule("First Schedule", university.getDegrees().get(0), university.getSemesters().get(1), (float)(1));
-				//ScheduleTester testschedule = new ScheduleTester(schedule, university);
-					
-				//System.out.println(university.generateSectionsFromNeededCourses(university.getDegrees().get(0), university.getSemesters().get(1), (float)(0.75)).size());
-				
-					
+
 					SystemJFrame frame = new SystemJFrame(university);
-					frame.setVisible(true);
-					
+
+					// Create the login Screen
+					LoginJDialog dialog = new LoginJDialog(frame,university );
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+					  
+					   if(dialog.isSucceeded()){
+						
+						   System.out.println("U can start the main frame");
+						frame.setVisible(true);
+				
+					}
+
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
